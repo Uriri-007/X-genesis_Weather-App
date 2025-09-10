f15a25428e30077b32f0889bd4a46a08779dd8bd
Uri Okhai <uriokhai@gmail.com>
2025-09-10 18:57:10 +0100

    Make the search button and input responsive, create the metric/imperial dropdown and configure the different apis for the different units

--- script.js
+++ script.js
@@ -1,56 +1,105 @@
 //IIFE for accessing DOM elements
 const Domaccess = (() => {
-  const  dropDowns  = document.querySelectorAll(".select-box")
-  const cityNameInput = document.querySelector("#city_name")
-  const searchBtn = document.querySelector("#search_btn")
-  const blueImgDiv = document.querySelector(".image-data")
-  const todaysDataDivs = document.querySelectorAll(".present-data > div")
-  const nextSevenDaysDivs = document.querySelectorAll(".daily-value > div")
-  const nextEightHrsDivs = document.querySelectorAll(".eight-hrs-data")
-  /*const  = document.querySelector("")
-  const  = document.querySelector("")
-  const  = document.querySelector("")
-  const  = document.querySelector("")
-  const  = document.querySelector("")
+    const dropDowns = document.querySelectorAll(".select-box");
+    const cityNameInput = document.querySelector("#city_name");
+    const searchBtn = document.querySelector("#search_btn");
+    const blueImgDiv = document.querySelector(".image-data");
+    const todaysDataSpans = document.querySelectorAll(".present-value");
+    const nextSevenDaysDivs = document.querySelectorAll(".daily-value > div");
+    const nextEightHrsDivs = document.querySelectorAll(".eight-hrs-data");
+    const cityDiv = document.querySelector(".place-name");
+    const dateSpan = document.querySelector(".current-date");
+    const todayIconImg = document.querySelector(".today-icon");
+    const maxTempSpan = document.querySelector(".max-temp");
+    /*const  = document.querySelector("")
   const  = document.querySelector("")*/
-  return {
-    dropDowns,
-    cityNameInput,
-    searchBtn,
-    blueImgDiv,
-    todaysDataDivs,
-    nextSevenDaysDivs,
-    nextEightHrsDivs
-  }
-})()
+    return {
+        dropDowns,
+        cityNameInput,
+        searchBtn,
+        blueImgDiv,
+        todaysDataSpans,
+        nextSevenDaysDivs,
+        nextEightHrsDivs,
+        cityDiv,
+        dateSpan,
+        todayIconImg,
+        maxTempSpan
+    };
+})();
+let isMetric = false;
 
+//Verify the user input
 function getValidUserData(string) {
-  if(!string) {
-    console.error("Place name cannot be empty")
-    return
-  }
-    return string.trim()
+    if (!string) {
+        console.error("Place name cannot be empty");
+        return;
+    }
+    return string.trim();
 }
 
+//Asynchronous functions for fetching data from Open Meteo APIs
 async function geoCodeUserData() {
-  const cityName = getValidUserData(Domaccess.cityNameInput.value)
-  return fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + cityName, { mode: "cors" })
+    const cityName = getValidUserData(Domaccess.cityNameInput.value);
+    try {
+        const rawCoordinates = await fetch(
+            "https://geocoding-api.open-meteo.com/v1/search?name=" + cityName,
+            { mode: "cors" }
+        );
+        const roughCoordinates = await rawCoordinates.json();
+        const coordinatesArray = roughCoordinates.results[0];
+        return coordinatesArray;
+    } catch (error) {
+        console.error("Error fetching data: " + error);
+    }
 }
-async function getCoordinates() {
-  const rawCoordinates = await geoCodeUserData()
-  const roughCoordinates = await rawCoordinates.json()
-  console.log(roughCoordinates)
+async function getWeatherData() {
+    const coordinatesArray = await geoCodeUserData();
+    //Configure the API for metric and imperial units
+    let rawWeatherData;
+    if (isMetric) {
+        rawWeatherData = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature&timezone=auto`,
+            { mode: "cors" }
+        );
+    } else {
+        rawWeatherData = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`,
+            { mode: "cors" }
+        );
+    }
+    console.log(rawWeatherData);
+    const roughData = await rawWeatherData.json();
+    console.log(roughData);
+    organizeRawData(roughData);
+}
+function organizeRawData(object) {
+    const { current } = object;
+    updateUI(current);
+}
+function updateUI(todays) {
+    const todaysData = [
+        todays.apparent_temperature,
+        todays.relative_humidity_2m,
+        todays.wind_speed_10m,
+        todays.precipitation
+    ];
+    todaysData.forEach((data, index) => {
+        Domaccess.todaysDataSpans[index].textContent = data;
+        Domaccess.maxTempSpan.textContent = todays.temperature_2m;
+    });
 }
 
-Domaccess.searchBtn.addEventListener("click", () => getCoordinates)
+function showLoadingStates() {}
 
-//Open Meteo APIs
-//—Weather API: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=wind_speed_10m,snowfall,showers,rain,is_day,precipitation,relative_humidity_2m,temperature_2m&timezone=auto
-//—Geocoding api: 
+Domaccess.searchBtn.addEventListener("click", () => {
+    console.log(true);
+    getWeatherData();
+});
 document.addEventListener("DOMContentLoaded", () => {
-  if(innerWidth > 1000) {
-    Domaccess.blueImgDiv.style.cssText = `background-image: url('./assets/images/bg-today-large.svg')`
-  } else {
-    Domaccess.blueImgDiv.style.cssText = `background-image: url('./assets/images/bg-today-small.svg')`
-  }
-})
+    if (innerWidth > 1000) {
+        Domaccess.blueImgDiv.style.cssText = `background-image: url('./assets/images/bg-today-large.svg')`;
+    } else {
+        Domaccess.blueImgDiv.style.cssText = `background-image: url('./assets/images/bg-today-small.svg')`;
+    }
+});
