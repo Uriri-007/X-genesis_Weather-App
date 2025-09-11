ef3a2a4184bc7c3a893856cd2f17826483861a3b
Uri Okhai <uriokhai@gmail.com>
2025-09-11 01:33:58 +0100

    Add the logic for handling the current weather icon

--- script.js
+++ script.js
@@ -63,12 +63,12 @@ async function getWeatherData() {
     let rawWeatherData;
     if (isMetric) {
         rawWeatherData = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature&timezone=auto`,
+            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m,weather_code&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature,weather_code&timezone=auto`,
             { mode: "cors" }
         );
     } else {
         rawWeatherData = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`,
+            `https://api.open-meteo.com/v1/forecast?latitude=${coordinatesArray.latitude}&longitude=${coordinatesArray.longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m,weather_code&current=wind_speed_10m,is_day,precipitation,relative_humidity_2m,temperature_2m,apparent_temperature,weather_code&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`,
             { mode: "cors" }
         );
     }
@@ -81,34 +81,76 @@ function organizeRawData(object) {
     updateUI(currentUnits, current);
 }
 function updateUI(units, todays) {
-  Domaccess.cityDiv.textContent = Domaccess.cityNameInput.value
+    Domaccess.cityDiv.textContent = Domaccess.cityNameInput.value;
     const todaysData = [
         Math.floor(todays.apparent_temperature) + "°",
         todays.relative_humidity_2m + units.relative_humidity_2m,
         todays.wind_speed_10m + " " + units.wind_speed_10m,
         todays.precipitation + " " + units.precipitation
     ];
-    Domaccess.maxTempSpan.textContent = Math.floor(todays.temperature_2m) + "°";
+    Domaccess.maxTempSpan.textContent = Math.floor(todays.temperature_2m) + "°"; 
+    Domaccess.todayIconImg.src = getSuitableIcon(todays.weather_code);
     todaysData.forEach((data, index) => {
         Domaccess.todaysDataSpans[index].textContent = data;
     });
 }
 function getCurrentDate() {
-  const date = new Date
-  const weekDay = date.getDay()
-  const monthDay = date.getDate()
-  const month = date.getMonth()
-  const year = date.getFullYear()
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  
-  return days[weekDay] + ", " + months[month] + " " + monthDay + ", " + year
-}
+    const date = new Date();
+    const weekDay = date.getDay();
+    const monthDay = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
 
+    return days[weekDay] + ", " + months[month] + " " + monthDay + ", " + year;
+}
+function getSuitableIcon(weatherCode) {
+        if([0, 1].some(code => code === weatherCode)) {
+            return "./assets/images/icon-sunny.webp";
+        } else if([2].some(code => code === weatherCode)) {
+            return "./assets/images/icon-partly-cloudy.webp";
+        } else if([3].some(code => code === weatherCode)) {
+            return "./assets/images/icon-overcast.webp";
+        } else if([45, 48].some(code => code === weatherCode)) {
+            return "./assets/images/icon-fog.webp";
+        } else if([51, 53, 55, 56, 57, 80].some(code => code === weatherCode)) {
+            return "./assets/images/icon-drizzle.webp";
+        } else if([61, 63, 65, 66, 67, 81, 82].some(code => code === weatherCode)) {
+            return "./assets/images/icon-rain.webp";
+        } else if([71, 73, 75, 77, 85, 86].some(code => code === weatherCode)) {
+            return "./assets/images/icon-snow.webp";
+        }else if([95, 96, 99].some(code => code === weatherCode)) {
+            return "./assets/images/icon-storm.webp";
+        } else {
+          return "./assets/images/icon-overcast.webp";
+    }
+}
 function showLoadingStates() {}
 function dropdownConversion() {
-  const conversionBox = Domaccess.toggleUnitElem.parentElement
-  conversionBox.classList.toggle("drop")
+    const conversionBox = Domaccess.toggleUnitElem.parentElement;
+    conversionBox.classList.toggle("drop");
 }
 document.addEventListener("DOMContentLoaded", () => {
     if (innerWidth > 1000) {
@@ -116,7 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         Domaccess.blueImgDiv.style.cssText = `background-image: url('./assets/images/bg-today-small.svg')`;
     }
-    Domaccess.dateSpan.textContent = getCurrentDate()
+    Domaccess.dateSpan.textContent = getCurrentDate();
 });
 Domaccess.searchBtn.addEventListener("click", () => getWeatherData());
-Domaccess.dropDowns[0].addEventListener("click", () => dropdownConversion())
+Domaccess.dropDowns[0].addEventListener("click", () => dropdownConversion());
